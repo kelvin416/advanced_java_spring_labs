024e71efac0bce10590e75d73e3332ca4c8d2013
kelvin416 <kelvinrotich416@gmail.com>
2023-10-18 16:18:54 +0300

    example autowired

---
+++ src/main/java/platform/codingnomads/co/corespring/examples/autowiredannotation/AMD.java
@@ -0,0 +1,7 @@
+package platform.codingnomads.co.corespring.examples.autowiredannotation;
+
+import org.springframework.stereotype.Component;
+
+@Component("amd")
+public class AMD implements Graphics{
+}
